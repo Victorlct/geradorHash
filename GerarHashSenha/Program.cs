7461abbc48c7b0edd682465d3aefe2a777d22f70
Victor Evangelista <victor.evangelista@accelera.com.br>
2024-11-18 21:18:59 -0300

    first commit

---
+++ GerarHashSenha/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+class Program
+{
+    static void Main()
+    {
+        var passwordHasher = new PasswordHasher<object>();
+        string senha = "teste";
+        string hashedPassword = passwordHasher.HashPassword(null, senha);
+        Console.WriteLine($"Hash da senha '{senha}': {hashedPassword}");
+    }
+}
